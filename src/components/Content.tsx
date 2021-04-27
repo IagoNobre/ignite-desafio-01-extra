8a08798aeba09dd16971699f791bf49271c44805
Iago Nobre <55515248+IagoNobre@users.noreply.github.com>
2021-04-26 21:47:42 -0300

    Update

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,28 @@
-export function Content() {
-  // Complete aqui
+import { ReactNode } from "react";
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface ContentProps {
+  children: ReactNode;
+  selectedGenre: GenreResponseProps;
+}
+
+export function Content({ children, selectedGenre }: ContentProps) {
+  return (
+    <div className="container">
+      <header>
+        <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {children}
+        </div>
+      </main>
+    </div>
+  )
 }
